394f6df8bcb0a1c5f21f4814f705467b6e8190ec
s-m33r <>
2024-10-16 15:41:23 +0530

    add Wishbone RAM slave connected to CPU FSM

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -44,15 +44,16 @@ module user_proj_example #(
 `endif
 
     // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
+    input         wb_clk_i,
+    input         wb_rst_i,
+
+    input         wbs_stb_i,
+    input         wbs_cyc_i,
+    input         wbs_we_i,
+    input   [3:0] wbs_sel_i,
+    input  [31:0] wbs_dat_i,
+    input  [31:0] wbs_adr_i,
+    output        wbs_ack_o,
     output [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
@@ -68,89 +69,116 @@ module user_proj_example #(
     // IRQ
     output [2:0] irq
 );
-    wire clk;
-    wire rst;
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
+wire [7:0] o_ram_data;
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
+reg [7:0] pc_reg;
+reg [7:0] a_reg;
+reg [7:0] b_reg;
+reg [7:0] alu_out;
+reg [7:0] io_reg;
+reg flag_carry;
+wire flag_zero = (alu_out == 8'b0);
 
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
+wb_ram RAM (
+	.i_clk    (wb_clk_i),
+	.i_reset  (wb_rst_i),
 
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
+	.i_wb_adr (wbs_adr_i),
+	.i_wb_dat (wbs_dat_i),
+	.i_wb_sel (wbs_sel_i),
+	.i_wb_we  (wbs_we_i ),
+	.i_wb_cyc (wbs_cyc_i),
+	.i_wb_stb (wbs_stb_i),
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
+	.o_wb_dat (wbs_dat_o),
+	.o_wb_ack (wbs_ack_o),
 
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+	.o_ram_data(o_ram_data),
+	.o_ram_addr(pc_reg    )
+);
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
-    );
+assign la_data_out[7:0] =  a_reg;
+assign la_data_out[13:8] = b_reg;
+assign la_data_out[15:14] = {flag_carry, flag_zero};
+assign la_data_out[23:16] = io_reg;
+
+always @(posedge wb_clk_i) begin
+	if (wb_rst_i) begin
+		a_reg = 0;
+		b_reg = 0;
+		pc_reg = 0;
+	end
+	else begin
+		case (o_ram_data)
+			8'b0: begin
+				$display("NOP");
+				pc_reg <= pc_reg + 1;
+			end
+		endcase
+	end
+end
 
 endmodule
 
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
+`default_nettype wire
+
+module wb_ram(
+    // RCC
+    input wire i_clk,
+    input wire i_reset,
+    // Wishbone
+    input  wire [31:0] i_wb_adr,
+    input  wire [31:0] i_wb_dat,
+    input  wire  [3:0] i_wb_sel,
+    input  wire        i_wb_we,
+    input  wire        i_wb_cyc,
+    input  wire        i_wb_stb,
+    output reg  [31:0] o_wb_dat,
+    output reg         o_wb_ack,
+
+	output [7:0]       o_ram_data,
+	input  [7:0]       o_ram_addr
 );
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
+// Number of 8-bit words to store
+localparam SIZE = 16;
+
+// Data storage
+reg [7:0] data [SIZE];
+
+assign o_ram_data = data[o_ram_addr];
+
+// Each data entry is 32 bits wide, so right shift the input address
+//localparam addr_width = $clog2(SIZE);
+localparam addr_width = 8;
+wire [addr_width-1:0] data_addr = i_wb_adr[addr_width+1:2];
+
+integer i;
+always @(posedge i_clk) begin
+
+    if (i_reset) begin
+        o_wb_ack <= 1'b0;
+    end else begin
+        o_wb_ack <= 1'b0;
+
+        if (i_wb_cyc & i_wb_stb & ~o_wb_ack) begin
+            // Always ack
+            o_wb_ack <= 1'b1;
+
+            // Reads
+            o_wb_dat <= data[data_addr];
+
+            // Writes
+            if (i_wb_we) begin
+                for (i = 0; i < 4; i++) begin
+                    if (i_wb_sel[i])
+                        data[data_addr][i*8 +: 8] <= i_wb_dat[i*8 +: 8];
+                end
             end
         end
     end
 
+end
+
 endmodule
-`default_nettype wire
